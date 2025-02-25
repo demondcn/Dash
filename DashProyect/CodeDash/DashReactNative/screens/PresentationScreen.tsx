ed3cdb5c8d59b2fb614444955d4bb03ba476892b
sebastian <seb._be@outlook.com>
2025-02-24 21:22:09 -0500

    presentacion

--- DashProyect/CodeDash/DashReactNative/screens/PresentationScreen.tsx
+++ DashProyect/CodeDash/DashReactNative/screens/PresentationScreen.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { View, Image, StyleSheet, Button, Text } from 'react-native';
+import { View, Image, StyleSheet, Button, Text, ImageBackground } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
-import { RootStackParamList } from '../types/navigation'; 
+import { RootStackParamList } from '../types/navigation';
 
 type Props = StackScreenProps<RootStackParamList, 'Presentation'>;
 
+const backgroundImageUri = 'https://i0.wp.com/www.marcelopedra.com.ar/blog/wp-content/uploads/2013/10/animated-gifs-of-fighting-game-backgrounds-7.gif?resize=624%2C396&ssl=1'; // Cambia esta URL por la de tu imagen
+
 const PresentationScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <ImageBackground source={{ uri: backgroundImageUri }} style={styles.container} resizeMode="cover">
       <View style={styles.imageContainer}>
         <Image 
-          source={{ uri: 'https://i.pinimg.com/736x/2f/6b/2b/2f6b2b1b133b0ffce51b861e51e759ff.jpg' }} // Reemplaza con tu URL
+          source={{ uri: 'https://i.pinimg.com/736x/2f/6b/2b/2f6b2b1b133b0ffce51b861e51e759ff.jpg' }} 
           style={styles.image}
           resizeMode="cover"
         />
       </View>
+
+      {/* Barra separadora */}
+      <View style={styles.separator} />
+
       <View style={styles.content}>
         <View style={styles.banner}>
-          <Text style={styles.bannerText}>IDK</Text>
+          <Text style={styles.bannerText}>
+            Descubre Dash, la tienda de juegos más eficiente y rápida del mercado. 
+            Encuentra los mejores títulos, ofertas exclusivas y una experiencia de compra sin igual.
+          </Text>
         </View>
       </View>
+
       <View style={styles.buttonContainer}>
         <Button title="Ver Juegos" onPress={() => navigation.navigate('MainTabs')} />
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -33,11 +43,16 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     width: '100%',
-    height: '30%',
+    height: '20%',
   },
   image: {
     width: '100%',
     height: '100%',
+  },
+  separator: {
+    width: '100%',
+    height: 25, // Ajusta el grosor de la barra
+    backgroundColor: '#2C3E50', // Color sólido para la barra
   },
   banner: {
     width: '90%',
@@ -53,10 +68,10 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
+    padding: 20,
   },
   content: {
     flex: 1,
-    backgroundColor: '#163A59',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -64,7 +79,8 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     paddingVertical: 20,
-    backgroundColor: '#163A59',
+    marginTop: 20,
+    backgroundColor: 'trasnparent',
   },
 });
 
